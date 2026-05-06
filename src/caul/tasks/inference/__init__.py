40e28b1b8a0dc89e72525f613ba757fb62faf0bd
Clément Doumouro <clement.doumouro@gmail.com>
2026-05-06 11:30:12 +0200

    refactor: force local import usage for optional deps

--- src/caul/tasks/inference/__init__.py
+++ src/caul/tasks/inference/__init__.py
@@ -1,19 +1,6 @@
-try:
-    from .parakeet import ParakeetInferenceRunner, ParakeetInferenceRunnerConfig
-except ModuleNotFoundError:
-    ParakeetInferenceRunner, ParakeetInferenceRunnerConfig = None, None
-try:
-    from .faster_whisper import (
-        FasterWhisperInferenceRunner,
-        FasterWhisperInferenceRunnerConfig,
-    )
-
-    WhisperCppInferenceRunner = FasterWhisperInferenceRunner
-    WhisperCppInferenceRunnerConfig = FasterWhisperInferenceRunnerConfig
-except ModuleNotFoundError:
-    FasterWhisperInferenceRunner, FasterWhisperInferenceRunnerConfig = None, None
-    WhisperCppInferenceRunner, WhisperCppInferenceRunnerConfig = None, None
-try:
-    from .fireredasr import FireRedASR2InferenceRunner, FireRedASR2InferenceRunnerConfig
-except ModuleNotFoundError:
-    FireRedASR2InferenceRunner, FireRedASR2InferenceRunnerConfig = None, None
+from .parakeet import ParakeetInferenceRunner, ParakeetInferenceRunnerConfig
+from .faster_whisper import (
+    FasterWhisperInferenceRunner,
+    FasterWhisperInferenceRunnerConfig,
+)
+from .fireredasr import FireRedASR2InferenceRunner, FireRedASR2InferenceRunnerConfig
